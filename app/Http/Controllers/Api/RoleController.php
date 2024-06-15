f808f9bb378224dc8c1a487e8cbdec330dd13504
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-06-15 22:55:56 +0700

    add show roles

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -14,4 +14,21 @@ class RoleController extends Controller
         $roles = Role::latest()->paginate(8);
         return new RoleResource(true, 'List Data Roles', $roles);
     }
+
+    public function show($id) 
+    {
+        try {
+            // Find role by ID
+            $role = Role::findOrFail($id);
+
+            // Return single Role as a resource
+            return new RoleResource(true, 'role Detail!', $role);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            // Return error response if role not found
+            return response()->json(['error' => 'role not found'], 404);
+        } catch (\Exception $e) {
+            // Return general error response for any other exceptions
+            return response()->json(['error' => 'An error occurred', 'message' => $e->getMessage()], 500);
+        }
+    }
 }
